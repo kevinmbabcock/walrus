41a934c9c3b93d7b55a536c45a55e549436f3830
Kevin Babcock and Qianqian Hu <kevin@>
2017-10-17 10:32:59 -0700

    add text and new feature to returnback to the start

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,17 @@
 $(function() {
+  $("#clickTheText").click(function() {
+    $(".text").show();
+    $(".first").show();
+  })
+
   $(".clickable").click(function() {
     $(".first").hide();
     $(".second").show();
   });
 
   $(".second .clickable").click(function() {
-    $(".first").show();
+    $(".text").hide();
+    $(".first").hide();
     $(".second").hide();
     $(".third").hide();
   })
