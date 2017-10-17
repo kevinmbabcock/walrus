7d7ca98e587e0c54d0ef6625aa8dd6ada3dd44d6
Kevin Babcock and Qianqian Hu <kevin@>
2017-10-17 09:50:48 -0700

    add third div with option to display picture before returning to home screen

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,22 @@
 $(function() {
   $(".clickable").click(function() {
-    $(".first").toggle();
-    $(".second").toggle();
+    $(".first").hide();
+    $(".second").show();
   });
+
+
+  $(".second .clickable").click(function() {
+    $(".first").show();
+    $(".second").hide();
+    $(".third").hide();
+  })
+
+  $(".shark").click(function() {
+    $(".third").show();
+  })
+
+
+
 
 
 
