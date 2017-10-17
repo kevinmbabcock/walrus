923fc2b3bff7bd6fd95655b5733371a65a425734
Kevin Babcock and Qianqian Hu <qianqian@>
2017-10-17 11:14:42 -0700

    add different background colors for user to choose from

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,28 @@
 $(function() {
   $("#clickTheText").click(function() {
-    $(".text").show();
+    $(".button").show();
+  });
+
+  $("button#green").click(function() {
+    $("body").removeClass();
+    $("body").addClass("green-background");
     $(".first").show();
-  })
+    $(".text").show();
+  });
+
+  $("button#yellow").click(function() {
+    $("body").removeClass();
+    $("body").addClass("yellow-background");
+    $(".first").show();
+    $(".text").show();
+  });
+
+  $("button#red").click(function() {
+    $("body").removeClass();
+    $("body").addClass("red-background");
+    $(".first").show();
+    $(".text").show();
+  });
 
   $(".clickable").click(function() {
     $(".first").hide();
@@ -10,15 +30,17 @@ $(function() {
   });
 
   $(".second .clickable").click(function() {
+    $("body").removeClass();
+    $(".button").hide();
     $(".text").hide();
     $(".first").hide();
     $(".second").hide();
     $(".third").hide();
-  })
+  });
 
-  $(".shark").click(function() {
+  $("#shark").click(function() {
     $(".third").slideToggle();
-  })
+  });
 
 
 });
